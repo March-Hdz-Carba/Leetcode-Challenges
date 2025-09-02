b98ef8dcffaff16adccbb25fa73e196338dc4ff0
March-Hdz-Carba <marcela.hdez.1801@gmail.com>
2025-09-02 12:43:40 -0600

    Second and better solution to Two-sum challenge

--- 1-Two-sum.js
+++ 1-Two-sum.js
@@ -20,3 +20,65 @@ const twoSum = (nums, target) => {
 };
 
 console.log(twoSum([2,7,11,15], 9));
+
+/**Esta es una segunda solucion, la cual tiene un mejor performace, y se utiliza un hasmap para buscar el valor
+ * 
+ * What is data we should keep with HashMap?
+Let's take a look at input array and target number.
+
+Input: nums = [2,7,11,15], target = 9
+In this case, if we subtract current number in input array from target number, we can get a number we need to create the target number.
+
+For example,
+
+9 - 2 = 7
+7 is a number we need when current number is 2. So if we have 7 and index number of 7, we can create 9 with the current(= 2) number and 7.
+
+From this example, what we should keep in HashMap is
+
+⭐️ Points
+
+Keep numbers(elements) as keys and index numbers as values in the input array.
+
+Let's iterate through one by one.
+
+Input: nums = [2,7,11,15], target = 9
+Let's call the HashMap pair_idx.
+
+[2,7,11,15], target = 9
+　　↑
+
+pair_idx = {} 
+We found 2. we need 7 to create 9, so check if we have 7 in pair_idx. But now pair_idx is empty, so just add current number and its index to pair_idx.
+
+pair_idx = {2: 0} 
+
+2 is current number
+0 is index of 2
+Then move next.
+
+[2,7,11,15], target = 9
+　　  ↑
+
+pair_idx = {2: 0}
+We found 7, so we need 2. Check if we have 2 in pair_idx. And yes we have 2.
+
+return [0,1] or [1,0]
+The description says "You can return the answer in any order".
+
+ */
+const twoSumOtherSolution = (nums, target) => {
+  const pairs = {};
+
+  for(let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i];
+    const searchingNum = target - currentNum;
+    if(searchingNum in pairs) {
+      return [i, pairs[searchingNum]];
+    }
+    pairs[currentNum] = i;
+  }
+}
+
+console.log(twoSumOtherSolution([2,7,11,15], 9));
+
